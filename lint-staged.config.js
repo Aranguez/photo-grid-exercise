6e16127b148b0823c2320baa2f6322df161bc620
Leandro Aranguez <leandro-aranguez@hotmail.com>
2023-02-22 21:44:27 -0300

    chore: add vitest, react-testing-library and eslint

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  "src/**/*.{ts,tsx}": ["eslint --max-warnings=0"]
+	'src/**/*.{ts,tsx}': ['eslint --max-warnings=0']
 }
---
+++ src/App.test.tsx
@@ -0,0 +1,8 @@
+import { render } from '@testing-library/react'
+import { it } from 'vitest'
+
+import App from './App'
+
+it('should render component correctly', () => {
+	render(<App />)
+})
