d1ab72531755fb3b977baa0b09c1b0c5bcac347c
Leandro Aranguez <leandro-aranguez@hotmail.com>
2023-02-24 02:00:03 -0300

    chore: add into vitest config coverage and tsconfigPaths

--- vitest.config.js
+++ vitest.config.js
@@ -1,10 +1,15 @@
 /// <reference types="vitest" />
 
 import { defineConfig } from 'vite'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
+  plugins: [tsconfigPaths()],
   test: {
     globals: true,
-    environment: 'happy-dom'
+    environment: 'happy-dom',
+    coverage: {
+      provider: 'c8' // or 'istanbul'
+    },
   }
 })
